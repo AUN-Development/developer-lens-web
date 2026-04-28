94bdf473571b46f085c1d6b310706297aa11c4dd
Solomon François <114085031+FrancoisSolomon@users.noreply.github.com>
2026-04-28 10:41:57 +0200

    wijziging font msans naar font mono

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist_Mono } from "next/font/google";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/ThemeProvider";
-
-const geist = Geist({
-  subsets: ["latin"],
-  variable: "--font-sans",
-});
 
 const geistMono = Geist_Mono({
   subsets: ["latin"],
@@ -25,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={cn("h-full antialiased", geist.variable, geistMono.variable)}>
+    <html lang="en" className={cn("h-full antialiased", geistMono.variable)}>
       <body className="min-h-full flex flex-col bg-background text-foreground">
         <ThemeProvider
           attribute="class"
